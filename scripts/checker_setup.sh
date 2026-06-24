f4b5db1444fac41e5f3c323b4cc6b366e3a2b035
Sivanandh C C <sivanandh.cc@xebia.com>
2026-06-24 12:29:18 +0530

    Use DNS name for NFS mount in checker_setup

--- scripts/checker_setup.sh
+++ scripts/checker_setup.sh
@@ -3,4 +3,4 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y -qq
 apt-get install -y -qq git nfs-common
 mkdir -p /root/todoapp
-mount -t nfs 10.0.230.4:/root/todoapp /root/todoapp 2>/dev/null || true
+mount -t nfs workstation.vm.sandbox.internal:/root/todoapp /root/todoapp 2>/dev/null || true
